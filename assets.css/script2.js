fa7ec5f61dfc0d75ebd5a10203749fa020cf851d
BreakfastSandwich <143359791+BreakfastSandwich@users.noreply.github.com>
2023-09-08 13:58:54 -0400

    added additional comments/wireframe for JS sheet.

--- assets.css/script2.js
+++ assets.css/script2.js
@@ -15,9 +15,10 @@ var passwordElement = document.querySelector('password')
 howManyNumElement.addEventListener('input', syncHowMany);
 howManySliderElement.addEventListener('input', syncHowMany);
 
-// stopping the "generate Password" button from submitting the form 
+// stopping the "generate Password" button from submitting the form and refreshing page 
     generate.addEventListener('submit', f=> {
     f.preventdefault()
+    // specifying values for future password array
     const charAmount = howManyNumElement.value
     const includeupper = upperCharsElement.checked
     const includeNum = numsElement.checked
@@ -26,10 +27,20 @@ howManySliderElement.addEventListener('input', syncHowMany);
     const password = generatePassword(charAmount, includeupper, includeNum, includeSpec)
 })
 
+// create array const to hold Ascii values for all Char types needed
+
+// creating the password array
 function generatePassword(charAmount, includeupper, includeNum, includeSpec){
 
-    
+    // create algo for the array length creation
+    // create randomizer to fill in the new array
+
 }
+
+// create function to convert the Ascii values to Chars
+// create print function to log the new password to the html file
+
+
 
 // creating function to sync the slider and numbers on the html side
 function syncHowMany (s) {
