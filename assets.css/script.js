8576a5ece91b6e0ff1e8cf1e0db4d7f6a190a096
BreakfastSandwich <143359791+BreakfastSandwich@users.noreply.github.com>
2023-09-07 21:55:19 -0400

    added user input box and some JS

---
+++ assets.css/script.js
@@ -0,0 +1,13 @@
+// use event listener to update Var's for character count, special characters, etc
+// use .onclick to start the password generation function and reset the user input values
+
+var count = prompt("How Many Characters Would You Like?","Min. 8 - Max 128")
+
+// create if else function to verify user input is between 8 and 128
+
+var special = confirm("Include Special Characters?")
+
+var upper = confirm("Include Uppercase Characters?")
+
+var numbers = confirm("Include Numbers?")
+
